2d9a54989c412ef9960b07bdc9f87fdcbd1b357f
陶建行 <1811783973@qq.com>
2024-11-15 17:55:59 +0800

    🐞 fix: 修复umd构建产物错误问题

--- rslib.config.ts
+++ rslib.config.ts
@@ -10,6 +10,9 @@ export default defineConfig(() => {
                 js: "maptalks.plotsymbol.js",
             },
             legalComments: "none",
+            externals: {
+                'maptalks':"var maptalks"
+            },
         },
         tools: {
             rspack: {
